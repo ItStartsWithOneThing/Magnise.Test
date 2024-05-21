1660c04248f1044f66db44c0340d169e4b8cd763
Kostiantyn Turkin <like69.kt@gmail.com>
2024-05-21 15:47:59 +0300

    Add project files.

---
+++ Magnise.Test/Controllers/KryptoController.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Magnise.Test.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KryptoController : ControllerBase
+    {
+    }
+}
